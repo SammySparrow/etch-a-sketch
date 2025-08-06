5975b619c2304577826f9dcd68c233c370b19213
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-08-06 19:56:13 +1000

    Implement button functionality

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const container = document.querySelector(".container");
-const gridSize = prompt("Select grid size");
+const newGrid = document.querySelector("#new-grid");
+const reset = document.querySelector("#reset");
 
-function pixelGrid() {
-  const height = 600 / gridSize;
-  const width = 600 / gridSize;
-  for (let i = 0; i < gridSize * gridSize; i++) {
+let gridSize = 16;
+
+function pixelGrid(size) {
+  const height = 600 / size;
+  const width = 600 / size;
+  for (let i = 0; i < size * size; i++) {
     const block = document.createElement("div");
     block.setAttribute("style", `height: ${height}px; width: ${width}px;`);
     container.appendChild(block);
   }
+
+  let squares = container.querySelectorAll("div");
+
+  squares.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+      e.target.style.background = "black";
+    });
+  });
+
+  reset.addEventListener("click", () => {
+    squares.forEach((div) => {
+      div.style.background = "";
+    });
+  });
 }
 
-pixelGrid();
+pixelGrid(gridSize);
 
-let squares = container.querySelectorAll("div");
-
-squares.forEach((div) => {
-  div.addEventListener("mouseover", (e) => {
-    e.target.style.background = "black";
-  });
+newGrid.addEventListener("click", () => {
+  container.replaceChildren();
+  gridSize = prompt("Enter new grid size (less than 100)");
+  while (gridSize > 100 || gridSize < 1) {
+    gridSize = prompt("Invalid choice, enter a new grid size");
+  }
+  pixelGrid(gridSize);
 });
