f03f39ba4b27a1fa0640373e4ef29301dbbec3b4
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-08-06 19:14:09 +1000

    Add event hover listener
    Added an event listener to change the background of
    each square to black when hovered.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
 const gridSize = prompt("Select grid size");
+
 function pixelGrid() {
-  const height = 960 / gridSize;
-  const width = 960 / gridSize;
+  const height = 600 / gridSize;
+  const width = 600 / gridSize;
   for (let i = 0; i < gridSize * gridSize; i++) {
     const block = document.createElement("div");
     block.setAttribute("style", `height: ${height}px; width: ${width}px;`);
@@ -11,3 +12,11 @@ function pixelGrid() {
 }
 
 pixelGrid();
+
+let squares = container.querySelectorAll("div");
+
+squares.forEach((div) => {
+  div.addEventListener("mouseover", (e) => {
+    e.target.style.background = "black";
+  });
+});
